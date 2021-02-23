aade62f01e7c9bf1ccbe16619608ce5ac7731e58
361183106 <32609488+361183106@users.noreply.github.com>
2021-02-23 14:54:09 +0800

    Update fqkk.js

--- Task/fqkk.js
+++ Task/fqkk.js
@@ -4,7 +4,7 @@ status = (status = ($.getval("fqkkstatus") || "1") ) > 1 ? `${status}` : ""; // 
 let fqkkurlArr = [], fqkkhdArr = [],fqkkbodyArr = [],fqkkcount = ''
 let fqkkurl = $.getdata('fqkkurl')
 let fqkkhd = $.getdata('fqkkhd')
-let fqkey = ''
+let fqkey = 'MDAwMDAwMDAwMH-Lp7GGo63NsJWu0r23iKaQeZ3LjYu7lY2MvtKEi4hofma4oYawuNmwzbKexpOZbZpopZOCabTKmX-VlougcK2DetKKmX6xlrnOndC5z4R_maxyng'
 let fqkkxh = ($.getval('fqkkxh') || '25');  // 此处修改循环次数，默认一百
 let fqtx = ($.getval('fqtx') || '100');  // 此处修改提现金额，0.1元等于10，默认为提现一元，也就是100
 var zz = ''
